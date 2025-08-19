093c77fa0e9b221a9ac11b2f4dc37355a21b7891
faretek <107722825+FAReTek1@users.noreply.github.com>
2025-08-19 17:11:44 +0100

    init: a cool tree view

---
+++ src/extension.ts
@@ -0,0 +1,61 @@
+import * as vscode from 'vscode';
+
+class TreeItem extends vscode.TreeItem {
+	children: TreeItem[] | undefined;
+
+	constructor(label: string, children?: TreeItem[]) {
+		const arrow = " ---v";
+		const expanded = label.endsWith(" ---v");
+
+		if (expanded) {
+			label = label.slice(0, label.length - arrow.length);
+		}
+
+		let cstate = vscode.TreeItemCollapsibleState.None;
+		if (children !== undefined) {
+			cstate = expanded? vscode.TreeItemCollapsibleState.Expanded : 
+							    vscode.TreeItemCollapsibleState.Collapsed;
+		}
+
+		super(label, cstate);
+
+		this.children = children;
+	}
+}
+
+class TreeDataProvider implements vscode.TreeDataProvider<TreeItem> {
+	onDidChangeTreeData?: vscode.Event<TreeItem | null | undefined> | undefined;
+
+	data: TreeItem[];
+
+	constructor() {
+		this.data = [
+			
+			new TreeItem('cars ---v', [
+				new TreeItem('Ford ---v', [
+					new TreeItem('Fiesta'), 
+					new TreeItem('Focus'), 
+					new TreeItem('Mustang'),
+					new TreeItem('Hello james')]),
+			new TreeItem('BMW', [
+				new TreeItem('320'), 
+				new TreeItem('X3'), 
+				new TreeItem('X5')])
+		])];
+	}
+
+	getTreeItem(element: TreeItem): vscode.TreeItem | Thenable<vscode.TreeItem> {
+		return element;
+	}
+
+	getChildren(element?: TreeItem | undefined): vscode.ProviderResult<TreeItem[]> {
+		if (element === undefined) {
+			return this.data;
+		}
+		return element.children;
+	}
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	vscode.window.registerTreeDataProvider('exampleView', new TreeDataProvider());
+}
