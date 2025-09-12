ae21f925bfb2c81b248cab3bd87a71f2dff5fb7c
faretek <107722825+FAReTek1@users.noreply.github.com>
2025-09-12 21:31:39 +0100

    feat: comment old code

--- src/extension.ts
+++ src/extension.ts
@@ -1,53 +1,55 @@
 import * as vscode from 'vscode';
-import * as tree from './tree';
-import * as sa from './sa/src/index';
-import * as site from './site';
+// import * as tree from './tree';
+// import * as sa from './sa/src/index';
+// import * as site from './site';
 
-import jsdom from 'jsdom';
+// import jsdom from 'jsdom';
 
 export function activate(context: vscode.ExtensionContext) {
-	vscode.commands.registerCommand("slatch.Tree.selectNode", (arg: tree.Item) => { arg.onClick(); });
+	// vscode.commands.registerCommand("slatch.Tree.selectNode", (arg: tree.Item) => { arg.onClick(); });
 
-	vscode.window.registerTreeDataProvider('slatch.commandView', new tree.DataProvider(context, [
-			new tree.Item('Home ---v', [
-				new tree.Item('Feed', (ti) => {
-					const panel = vscode.window.createWebviewPanel(
-						'slatchFeed',
-						'Slatch: Feed',
-						vscode.ViewColumn.One,
-						{}
-					);
+	// vscode.window.registerTreeDataProvider('slatch.commandView', new tree.DataProvider(context, [
+	// 		new tree.Item('Home ---v', [
+	// 			new tree.Item('Feed', (ti) => {
+	// 				const panel = vscode.window.createWebviewPanel(
+	// 					'slatchFeed',
+	// 					'Slatch: Feed',
+	// 					vscode.ViewColumn.One,
+	// 					{}
+	// 				);
 					
-					let dom: jsdom.JSDOM;
-					[panel.webview.html, dom] = site.getDom('feed.html');
+	// 				let dom: jsdom.JSDOM;
+	// 				[panel.webview.html, dom] = site.getDom('feed.html');
 
-					sa.getFeatured().then((data) => {
-						function gendom(id: string) {
-							const div = dom.window.document.querySelector(`div[id=${id}]`);
-							if (div) {
-								div.innerHTML = site.generateBscHTML(Object(data)[id], '120', 'height="90"');
-							}
-						}
+	// 				sa.getFeatured().then((data) => {
+	// 					function gendom(id: string) {
+	// 						const div = dom.window.document.querySelector(`div[id=${id}]`);
+	// 						if (div) {
+	// 							div.innerHTML = site.generateBscHTML(Object(data)[id], '120', 'height="90"');
+	// 						}
+	// 					}
 
-						gendom('community_newest_projects');
-						gendom('community_most_remixed_projects');
-						gendom('scratch_design_studio');
-						gendom('curator_top_projects');
-						gendom('community_most_loved_projects');
-						gendom('community_featured_projects');
-						gendom('community_featured_studios');
+	// 					gendom('community_newest_projects');
+	// 					gendom('community_most_remixed_projects');
+	// 					gendom('scratch_design_studio');
+	// 					gendom('curator_top_projects');
+	// 					gendom('community_most_loved_projects');
+	// 					gendom('community_featured_projects');
+	// 					gendom('community_featured_studios');
 
-						panel.webview.html = dom.serialize();
-						console.log(panel.webview.html);
-					});
-				}),
-				new tree.Item('Featured Projects'),
-				new tree.Item('Featured Studios'),
-				new tree.Item('Turbowarp Featured Projects'),
-				new tree.Item('Scratch Design Studio'),
-				new tree.Item('Projects Loved by Scratchers I\'m Following'),
-				new tree.Item('Top Remixed'),
-				new tree.Item('Top Loved'),
-			])
-		]));
+	// 					panel.webview.html = dom.serialize();
+	// 					console.log(panel.webview.html);
+	// 				});
+	// 			}),
+	// 			new tree.Item('Featured Projects'),
+	// 			new tree.Item('Featured Studios'),
+	// 			new tree.Item('Turbowarp Featured Projects'),
+	// 			new tree.Item('Scratch Design Studio'),
+	// 			new tree.Item('Projects Loved by Scratchers I\'m Following'),
+	// 			new tree.Item('Top Remixed'),
+	// 			new tree.Item('Top Loved'),
+	// 		])
+	// 	]));
+
+	
 }
